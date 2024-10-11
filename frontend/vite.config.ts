dc30db14d8aed004f9cce3ed0023de063d344e3d
MauiricioLesa <maulsalles@gmail.com>
2024-10-10 20:11:49 -0400

    cofiguration of docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 import React from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [React()]
+    plugins: [React()],
+    server: {
+        port: 5173,
+        host: true,
+        watch: {
+           usePolling: true,
+        },
+    }
+    
 })
